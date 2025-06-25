f49a3d8b1f11a7987b73542ab9fb33799e40be07
Sri Ganesh Thota <sriganeshthota12345@gmail.com>
2025-06-25 22:54:51 +0530

    Added initial db

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS urls (
+    id SERIAL PRIMARY KEY,
+    original_url TEXT NOT NULL,
+    short_code VARCHAR(10) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    custom_alias BOOLEAN DEFAULT FALSE
+);
