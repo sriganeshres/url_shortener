9a6c27e1acc1da9c2fb3424a5803871579f28d2e
Sri Ganesh Thota <sriganeshthota12345@gmail.com>
2025-06-25 22:52:55 +0530

    Added handlers

--- handlers/redirect.go
+++ handlers/redirect.go
@@ -1,1 +1,24 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sriganeshres/url_shortener/db"
+	"github.com/sriganeshres/url_shortener/kafka"
+)
+
+func RedirectURL(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	shortCode := vars["shortCode"]
+
+	originalURL, err := db.GetOriginalURL(shortCode)
+	if err != nil {
+		http.Error(w, "URL not found", http.StatusNotFound)
+		return
+	}
+
+	go kafka.LogVisit(shortCode, r.RemoteAddr)
+
+	http.Redirect(w, r, originalURL, http.StatusFound)
+}
--- handlers/shorten.go
+++ handlers/shorten.go
@@ -1,1 +1,38 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/sriganeshres/url_shortener/db"
+	"github.com/sriganeshres/url_shortener/utils"
+)
+
+type ShortenRequest struct {
+	URL         string `json:"url"`
+	CustomAlias string `json:"customAlias"`
+}
+
+func ShortenURL(w http.ResponseWriter, r *http.Request) {
+	var req ShortenRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil || req.URL == "" {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	shortCode := req.CustomAlias
+	if shortCode == "" {
+		shortCode = utils.GenerateShortCode()
+	}
+
+	err = db.SaveURL(req.URL, shortCode, req.CustomAlias != "")
+	if err != nil {
+		http.Error(w, "Could not save URL", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"shortUrl": "http://localhost:8080/" + shortCode,
+	})
+}
