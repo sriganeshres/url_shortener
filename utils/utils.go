e7fba8ee8a25c388e2455b24894a4b18f289e588
Sri Ganesh Thota <sriganeshthota12345@gmail.com>
2025-06-25 22:53:10 +0530

    Added Utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,15 @@
 package utils
+
+import (
+    "math/rand"
+)
+
+const charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func GenerateShortCode() string {
+    b := make([]byte, 6)
+    for i := range b {
+        b[i] = charset[rand.Intn(len(charset))]
+    }
+    return string(b)
+}
